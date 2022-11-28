36a121572e23133206a49808cb8933a83c3dfb7d
Dhanush Tipparaju <dhanushtips@gmail.com>
2022-11-28 09:57:11 -0600

    Initial Commit

---
+++ Cool Soundboard Mr.B/Cool_Soundboard_Mr_BApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cool_Soundboard_Mr_BApp.swift
+//  Cool Soundboard Mr.B
+//
+//  Created by Dhanush Tipparaju on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Cool_Soundboard_Mr_BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
